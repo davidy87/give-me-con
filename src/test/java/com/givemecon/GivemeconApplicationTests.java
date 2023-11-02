6dfeaf7aaabbe64ce4b126a4204e2467b3046664
Jungwoong (David) Yoon <dyoon0807@gmail.com>
2023-11-03 01:21:28 +0900

    initial commit

---
+++ src/test/java/com/givemecon/GivemeconApplicationTests.java
@@ -0,0 +1,13 @@
+package com.givemecon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GivemeconApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
