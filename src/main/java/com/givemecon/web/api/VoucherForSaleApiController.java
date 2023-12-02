83088ae26414c18bb9a8b7322755bd68e35a1a12
Jungwoong (David) Yoon <dyoon0807@gmail.com>
2023-12-03 03:14:57 +0900

    [#2] fix: VoucherForSale API 수정 - save() 에서, 로그인 사용자 이름 확인 방법 변경

--- src/main/java/com/givemecon/web/api/VoucherForSaleApiController.java
+++ src/main/java/com/givemecon/web/api/VoucherForSaleApiController.java
@@ -1,11 +1,12 @@
 package com.givemecon.web.api;
 
+import com.givemecon.config.auth.jwt.JwtTokenProvider;
 import com.givemecon.domain.voucher.VoucherForSaleService;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 
 import static com.givemecon.web.dto.VoucherDto.*;
@@ -19,13 +20,17 @@ public class VoucherForSaleApiController {
 
     private final VoucherForSaleService voucherForSaleService;
 
+    private final JwtTokenProvider jwtTokenProvider;
+
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping
-    public VoucherForSaleResponse save(@AuthenticationPrincipal OAuth2User principal,
+    public VoucherForSaleResponse save(HttpServletRequest request,
                                        @RequestBody VoucherForSaleRequest requestDto) {
 
-        log.info("username = {}", principal.getName());
-        return voucherForSaleService.save(principal.getName(), requestDto);
+        String accessToken = jwtTokenProvider.retrieveToken(request);
+        Authentication authentication = jwtTokenProvider.getAuthentication(accessToken);
+
+        return voucherForSaleService.save(authentication.getName(), requestDto);
     }
 
     @DeleteMapping("/{id}")
