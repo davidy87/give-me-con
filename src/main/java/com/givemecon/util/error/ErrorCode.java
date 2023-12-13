6163714bb8fd84508b313f838349c2f218e23cb4
Jungwoong (David) Yoon <dyoon0807@gmail.com>
2023-12-14 02:53:36 +0900

    [#8] fix: Invalid Token 오류 응답 코드 수정 - 403 Forbidden -> 401 Unauthorized

--- src/main/java/com/givemecon/util/error/ErrorCode.java
+++ src/main/java/com/givemecon/util/error/ErrorCode.java
@@ -9,7 +9,7 @@ import org.springframework.http.HttpStatus;
 public enum ErrorCode {
 
     NOT_FOUND("001", HttpStatus.NOT_FOUND.value(), "Entity Not Found (존재하지 않는 Entity입니다)."),
-    TOKEN_EXPIRED("002", HttpStatus.FORBIDDEN.value(), "Access Token Expired (기간이 만료된 토큰입니다).");
+    TOKEN_EXPIRED("002", HttpStatus.UNAUTHORIZED.value(), "Access Token Expired (기간이 만료된 토큰입니다).");
 
     private String code;
     private int status;
