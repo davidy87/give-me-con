fc26871ac5f196451b05ec1491cd6f80d99f557a
Jungwoong (David) Yoon <dyoon0807@gmail.com>
2023-12-13 23:37:10 +0900

    [#8] feat: JwtExceptionFilter 추가

---
+++ src/main/java/com/givemecon/config/auth/jwt/JwtExceptionFilter.java
@@ -0,0 +1,44 @@
+package com.givemecon.config.auth.jwt;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.givemecon.util.error.ErrorCode;
+import com.givemecon.util.error.ErrorResponse;
+import io.jsonwebtoken.ExpiredJwtException;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Slf4j
+public class JwtExceptionFilter extends OncePerRequestFilter {
+
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request,
+                                    HttpServletResponse response,
+                                    FilterChain filterChain) throws ServletException, IOException {
+
+        try {
+            filterChain.doFilter(request, response);
+        } catch (ExpiredJwtException e) {
+            log.info("Access Token 만료");
+
+            ErrorResponse errorResponse = ErrorResponse.builder()
+                    .status(HttpStatus.FORBIDDEN.name())
+                    .code(ErrorCode.TOKEN_EXPIRED.getCode())
+                    .message(ErrorCode.TOKEN_EXPIRED.getMessage())
+                    .build();
+
+            response.setStatus(HttpStatus.FORBIDDEN.value());
+            response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+            response.setCharacterEncoding("UTF-8");
+            response.getWriter().write(new ObjectMapper().writeValueAsString(errorResponse));
+        }
+    }
+}
