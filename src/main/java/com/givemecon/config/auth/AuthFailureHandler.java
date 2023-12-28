004a70faa186d800192340d5a9b4f5e61304f637
Jungwoong (David) Yoon <dyoon0807@gmail.com>
2023-12-28 23:55:38 +0900

    refactor: AuthFailureHandler 수정 -> redirect uri에 로그인 실패 이유 추가

--- src/main/java/com/givemecon/config/auth/AuthFailureHandler.java
+++ src/main/java/com/givemecon/config/auth/AuthFailureHandler.java
@@ -3,7 +3,6 @@ package com.givemecon.config.auth;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
@@ -17,19 +16,16 @@ public class AuthFailureHandler implements AuthenticationFailureHandler {
 
     private static final String ERROR_PATH = "http://localhost:8081/login?error=true";
 
-    private static final String OAUTH_ERROR_PATH = "http://localhost:8081/login?oauth2Error=true";
-
-
     @Override
     public void onAuthenticationFailure(HttpServletRequest request,
                                         HttpServletResponse response,
                                         AuthenticationException exception) throws IOException {
 
-        if (exception instanceof BadCredentialsException) {
-            response.sendRedirect(ERROR_PATH);
-        } else if (exception instanceof OAuth2AuthenticationException) {
+        if (exception instanceof OAuth2AuthenticationException) {
             log.info("--- In AuthFailureHandler ---");
-            response.sendRedirect(OAUTH_ERROR_PATH);
+            response.sendRedirect(ERROR_PATH + "&state=duplicate-email");
+        } else {
+            response.sendRedirect(ERROR_PATH);
         }
     }
 }
