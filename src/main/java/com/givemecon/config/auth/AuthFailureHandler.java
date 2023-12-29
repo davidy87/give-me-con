e9e0555fefd8d87f645be04f8cd7f49320bd6b10
Jungwoong (David) Yoon <dyoon0807@gmail.com>
2023-12-29 19:44:02 +0900

    refactor: AuthFailureHandler 수정 - redirect uri 생성 방법 수정

--- src/main/java/com/givemecon/config/auth/AuthFailureHandler.java
+++ src/main/java/com/givemecon/config/auth/AuthFailureHandler.java
@@ -7,25 +7,31 @@ import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.stereotype.Component;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 
 @Slf4j
 @Component
 public class AuthFailureHandler implements AuthenticationFailureHandler {
 
-    private static final String ERROR_PATH = "http://localhost:8081/login?error=true";
+    private static final String LOGIN_URI = "http://localhost:8081/login";
 
     @Override
     public void onAuthenticationFailure(HttpServletRequest request,
                                         HttpServletResponse response,
                                         AuthenticationException exception) throws IOException {
 
+        UriComponentsBuilder uriBuilder = UriComponentsBuilder.fromUriString(LOGIN_URI)
+                .encode(StandardCharsets.UTF_8)
+                .queryParam("error", true);
+
         if (exception instanceof OAuth2AuthenticationException) {
             log.info("--- In AuthFailureHandler ---");
-            response.sendRedirect(ERROR_PATH + "&state=duplicate-email");
-        } else {
-            response.sendRedirect(ERROR_PATH);
+            uriBuilder.queryParam("state", "duplicate-email");
         }
+
+        response.sendRedirect(uriBuilder.toUriString());
     }
 }
