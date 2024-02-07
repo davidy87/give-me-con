f680337bb99ea61ed1ac7cf03270c4f79a6914e6
Jungwoong (David) Yoon <dyoon0807@gmail.com>
2024-02-07 23:57:39 +0900

    [#18] fix: Image entity들의 컬럼 중복을 해결하기 위해 ImageEntity를 생성

---
+++ src/main/java/com/givemecon/domain/ImageEntity.java
@@ -0,0 +1,32 @@
+package com.givemecon.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.MappedSuperclass;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@MappedSuperclass
+public abstract class ImageEntity extends BaseTimeEntity {
+
+    @Column(nullable = false, unique = true)
+    private String imageKey;
+
+    @Column(nullable = false, unique = true)
+    private String imageUrl;
+
+    @Column(nullable = false)
+    private String originalName;
+
+    protected ImageEntity(String imageKey, String imageUrl, String originalName) {
+        this.imageKey = imageKey;
+        this.imageUrl = imageUrl;
+        this.originalName = originalName;
+    }
+
+    public abstract void update(String imageUrl, String originalName);
+}
