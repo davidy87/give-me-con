324bffffc9689e72f69d63632be2ec297258704c
Jungwoong (David) Yoon <dyoon0807@gmail.com>
2024-03-22 20:31:53 +0900

    [#20] feat: TokenReissueController API 문서화 적용

--- src/test/java/com/givemecon/controller/jwt/TokenReissueControllerTest.java
+++ src/test/java/com/givemecon/controller/jwt/TokenReissueControllerTest.java
@@ -11,9 +11,9 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.restdocs.RestDocumentationContextProvider;
 import org.springframework.restdocs.RestDocumentationExtension;
+import org.springframework.restdocs.payload.JsonFieldType;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
@@ -23,23 +23,24 @@ import org.springframework.web.context.WebApplicationContext;
 
 import static com.givemecon.config.auth.enums.JwtAuthHeader.*;
 import static com.givemecon.config.auth.enums.Role.*;
+import static com.givemecon.controller.ApiDocumentUtils.getDocumentRequest;
+import static com.givemecon.controller.ApiDocumentUtils.getDocumentResponse;
 import static com.givemecon.controller.TokenHeaderUtils.*;
 import static com.givemecon.domain.member.MemberDto.*;
 import static org.assertj.core.api.Assertions.*;
-import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.*;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration;
+import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
+import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
 import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@Transactional
 @ExtendWith({RestDocumentationExtension.class, SpringExtension.class})
-@SpringBootTest(webEnvironment = RANDOM_PORT)
+@Transactional
+@SpringBootTest
 public class TokenReissueControllerTest {
-
-    @LocalServerPort
-    int port;
 
     @Autowired
     WebApplicationContext context;
@@ -75,13 +76,12 @@ public class TokenReissueControllerTest {
     }
 
     @Test
-    void reissueAccessToken() throws Exception {
+    void reissueToken() throws Exception {
         // given
-        String url = "http://localhost:" + port + "/api/auth/reissue";
         Claims oldClaims = jwtUtils.getClaims(tokenInfo.getAccessToken());
 
         // when
-        ResultActions response = mockMvc.perform(get(url)
+        ResultActions response = mockMvc.perform(get("/api/auth/reissue")
                 .header(AUTHORIZATION.getName(), getAccessTokenHeader(tokenInfo))
                 .header(REFRESH_TOKEN.getName(), getRefreshTokenHeader(tokenInfo)));
 
@@ -94,5 +94,17 @@ public class TokenReissueControllerTest {
         TokenInfo newTokenInfo = new ObjectMapper().readValue(responseString, TokenInfo.class);
         Claims newClaims = jwtUtils.getClaims(newTokenInfo.getAccessToken());
         assertThat(newClaims.get("username")).isEqualTo(oldClaims.get("username"));
+
+        // API Documentation
+        response.andDo(document("{class-name}/{method-name}",
+                    getDocumentRequest(),
+                    getDocumentResponse(),
+                    responseFields(
+                            fieldWithPath("grantType").type(JsonFieldType.STRING).description("Token의 grant type"),
+                            fieldWithPath("accessToken").type(JsonFieldType.STRING).description("Access Token"),
+                            fieldWithPath("refreshToken").type(JsonFieldType.STRING).description("Refresh Token"),
+                            fieldWithPath("role").type(JsonFieldType.STRING).description("Member의 role")
+                    ))
+                );
     }
 }
