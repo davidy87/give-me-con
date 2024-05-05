c54703e730b1bd2bc3d456dfe5ac4cb6f51872e7
Jungwoong (David) Yoon <dyoon0807@gmail.com>
2024-05-06 03:56:00 +0900

    refactor: Category controller & DTO 수정
    - update가 @ModelAttribute를 사용한 DTO를 받도록 변경
    
    - CategoryDto의 CategorySaveRequest와 CategoryUpdateRequest에 builder 대신 생성자 추가

--- src/main/java/com/givemecon/domain/category/CategoryDto.java
+++ src/main/java/com/givemecon/domain/category/CategoryDto.java
@@ -2,17 +2,14 @@ package com.givemecon.domain.category;
 
 import com.givemecon.util.validator.ValidFile;
 import jakarta.validation.constraints.NotBlank;
-import lombok.AccessLevel;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 import org.springframework.web.multipart.MultipartFile;
 
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public final class CategoryDto {
 
     @Getter
-    @Builder
+    @RequiredArgsConstructor
     public static class CategorySaveRequest {
 
         @NotBlank
@@ -29,7 +26,7 @@ public final class CategoryDto {
     }
 
     @Getter
-    @Builder
+    @RequiredArgsConstructor
     public static class CategoryUpdateRequest {
 
         private final String name;
