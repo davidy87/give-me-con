9f6a2578f8e142c2ea2f349a462576484f09aa0a
Jungwoong (David) Yoon <dyoon0807@gmail.com>
2024-05-26 22:45:28 +0900

    refactor: 인증 성공 후 로직 수정
    - 인증 성공 시, authorization code를 redirect uri 파라미터에 포함하도록 변경
    
    - 클라이언트가 인증 성공 후 토큰 발급 요청을 보낼 때, authorization code를 파라미터로 전달하도록 변경

--- src/main/java/com/givemecon/config/enums/SessionAttribute.java
+++ src/main/java/com/givemecon/config/enums/SessionAttribute.java
@@ -7,9 +7,7 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public enum SessionAttribute {
 
-    TOKEN_INFO("tokenInfo", 10);
-
-    private final String name;
+    TOKEN_INFO(10);
 
     private final int duration;
 }
