a0177ebc66d4c06ae0d4cf06fbc7e44435a5e17f
davidy87 <dyoon0807@gmail.com>
2024-07-29 00:55:14 +0900

    refactor: Admin API 분리에 따른 ApiPathPattern & RequestMatcherList 수정

--- src/main/java/com/givemecon/common/auth/util/RequestMatcherList.java
+++ src/main/java/com/givemecon/common/auth/util/RequestMatcherList.java
@@ -30,16 +30,13 @@ public final class RequestMatcherList {
 
     public static RequestMatcher[] ofRoleAdmin() {
         return new AntPathRequestMatcher[] {
-                antMatcher(MEMBER_API.getPattern()),
-                antMatcher(CATEGORY_API.getPattern()),
-                antMatcher(BRAND_API.getPattern()),
-                antMatcher(VOUCHER_KIND_API.getPattern()),
                 antMatcher(ADMIN_API.getPattern())
         };
     }
 
     public static RequestMatcher[] ofAnyRole() {
         return new AntPathRequestMatcher[] {
+                antMatcher(MEMBER_API.getPattern()),
                 antMatcher(TOKEN_REISSUE_API.getPattern()),
                 antMatcher(VOUCHER_API.getPattern()),
                 antMatcher(LIKED_VOUCHER_API.getPattern()),
