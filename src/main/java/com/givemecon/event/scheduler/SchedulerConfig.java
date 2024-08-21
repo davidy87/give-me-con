02d0f9e27bdc2c785518dcff5b38ce8b89e1327d
davidy87 <dyoon0807@gmail.com>
2024-08-22 01:15:44 +0900

    fix: 기능 오류 수정
    - VoucherDto.StatusUpdateRequest의 statusCode 검증 범위 수정
    
    - SchedulerConfig에 누락된 @Configuration 어노테이션 추가

--- src/main/java/com/givemecon/event/scheduler/SchedulerConfig.java
+++ src/main/java/com/givemecon/event/scheduler/SchedulerConfig.java
@@ -1,7 +1,9 @@
 package com.givemecon.event.scheduler;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableScheduling
+@Configuration
 public class SchedulerConfig {
 }
